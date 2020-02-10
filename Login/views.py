6a4b606f80219a6bf1c2b17d074f7274f8eb62e0
Luis Matuz <lmatuz2211@gmail.com>
2020-02-10 10:22:09 -0600

    Creating model/serializer Example2

--- Login/views.py
+++ Login/views.py
@@ -1,6 +1,20 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+
+#Importación de librerías para errores u obtener objetos
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
+#Importación de librerías para API Rest
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status, generics
+
+#Importación de modelo
+from Login.models import Example2
+
+#Importación de serializer
+from Login.serializer import Example2Serializer
 
 class CustomAuthToken(ObtainAuthToken):
     
@@ -15,8 +29,18 @@ class CustomAuthToken(ObtainAuthToken):
         token, created = Token.objects.get_or_create(user=user)
 
         return Response({
-            'error_status': 'IT WORKED! Keep up the great work!',
+            'it_works': 'IT WORKED! Keep up the great work!',
             'token': token.key,
             'user_id': user.pk,
             'username': user.username
         })
+
+class Example2List(APIView):
+    def get(self,request,format=None):
+        print("Realizando consulta...")
+        queryset = Example2.objects.filter(delete=False)
+        #many = True #(Solo si se van a retornar múltiples objetos)
+        serializer = Example2Serializer(queryset,many=True)
+        return Response(serializer.data)
+
+    
